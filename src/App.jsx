0af412f171fac93127201ef0bca22387a4445741
Matheus <mathlegeferi@gmail.com>
2025-04-16 23:17:54 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [cardapio, setCardapio] = useState(["Coxinha","Pastel", "Pizza", "Charles"])
+  const [cardapio, setCardapio] = useState([{nome : "Coxinha", preco : '5,00'}])
 
   return (
     <>
     <form>
     {cardapio.map((item) => {
       return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center' , width: '500px'}}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlOVPOFvPb5zuHi8lEc_D0-il0GYmnlYN8Dg&s'
         style={{ width: '100px', height: '100px', marginRight: '16px' }}></img>
-        <h2>{item}</h2>
+        <h2>{item.nome}</h2>
         <div style={{marginLeft: 'auto'}}>
           Encomendar <br />
         <button>-</button>number <button>+</button>
